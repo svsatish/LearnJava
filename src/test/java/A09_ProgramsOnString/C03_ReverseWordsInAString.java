5ede91d8a20387dcb1662a83dea23046b9b30c7f
satish <satishatgmu@gmail.com>
2025-04-12 16:57:30 -0400

    Reverse words in a String

---
+++ src/test/java/A09_ProgramsOnString/C03_ReverseWordsInAString.java
@@ -0,0 +1,24 @@
+package A09_ProgramsOnString;
+
+import java.util.Scanner;
+
+public class C03_ReverseWordsInAString {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your first and last name: ");
+        String str = scanner.nextLine();
+
+        reverseWordsInAString(str);
+    }
+
+    static void reverseWordsInAString(String str){
+
+        String words[] = str.split(" ");
+
+        for(int i= words.length-1; i>=0; i--){
+            System.out.print(words[i] + " ");
+        }
+    }
+}
